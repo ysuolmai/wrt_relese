7b0e0c85bd6836b144797e8ee822b4dd9366bfe9
ysuolmai <fan0417@gmail.com>
2025-05-24 18:37:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -97,7 +97,7 @@ UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 keywords_to_delete=(
     "xiaomi_ax3600" "xiaomi_ax9000" "xiaomi_ax1800" "glinet" "jdcloud_ax6600"
     "mr7350" "uugamebooster" "luci-app-wol" "luci-i18n-wol-zh-cn" "CONFIG_TARGET_INITRAMFS" "ddns" "LSUSB" "mihomo"
-    "smartdns" "kucat" "bootstrap" "mosdns" "quickstart"
+    "smartdns" "kucat" "bootstrap" "mosdns" "quickstart" "istore"
 )
 
 
