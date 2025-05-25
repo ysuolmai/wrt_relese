e64208b755f66bafe2a8919faa2fd606bf728b68
ysuolmai <fan0417@gmail.com>
2025-05-25 09:20:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -101,7 +101,7 @@ keywords_to_delete=(
 )
 
 
-[[ $FIRMWARE_TAG == *"NOWIFI"* ]] && keywords_to_delete+=("wpad" "hostapd")
+[[ $FIRMWARE_TAG == *"NOWIFI"* ]] && keywords_to_delete+=("wpa" "hostapd" "wifi" "wireless")
 #[[ $FIRMWARE_TAG != *"EMMC"* ]] && keywords_to_delete+=("samba" "autosamba" "jdcloud_ax1800-pro" "redmi_ax5-jdcloud")
 #[[ $FIRMWARE_TAG != *"EMMC"* ]] && keywords_to_delete+=("samba" "autosamba" "disk")
 #[[ $FIRMWARE_TAG == *"EMMC"* ]] && keywords_to_delete+=("cmiot_ax18" "qihoo_v6" "redmi_ax5=y" "zn_m2")
